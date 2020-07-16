08d97a679518d2480518abb8cb193007b7a1ddd6
Ottó Radics <otto@webmenedzser.hu>
2020-07-16 08:32:08 +0200

    Added PHP header

--- src/services/ResultService.php
+++ src/services/ResultService.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Simplepay for Craft Commerce
+ *
+ * SimplePay payment gateway for Craft Commerce
+ *
+ * @link      https://www.webmenedzser.hu
+ * @copyright Copyright (c) 2020 Ottó Radics
+ */
 
 namespace webmenedzser\craftsimplepay\services;
 
